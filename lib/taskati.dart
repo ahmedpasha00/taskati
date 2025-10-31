60020acaac37b9ed35ef1f013ee32edbb223462e
ahmedpasha00 <ahmedpasha@example.com>
2025-10-31 17:52:00 +0200

    first commit

---
+++ lib/taskati.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class Taskati extends StatelessWidget {
+  const Taskati({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
+  }
+}
